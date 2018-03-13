b150b4ed2010940d976e8397603aeb9a0aeed49a
dnsuresh <32706072+dnsuresh@users.noreply.github.com>
2018-03-13 17:20:31 +0530

    basic project structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { LoginModule } from './login/login.module';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    LoginModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
